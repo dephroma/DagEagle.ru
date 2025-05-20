be180f6f15769766e88ca91aba33de5968a18ad0
RoboRabbit <52580643+dephroma@users.noreply.github.com>
2025-05-21 01:14:18 +0300

    51515

--- script.js
+++ script.js
@@ -146,6 +146,15 @@ document.getElementById('phone').addEventListener('input', function(e) {
             });
         });
 
+        window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+    } else {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+    }
+});
+
 
 
 
